7864dc5673a9ddd36b3dd35a96d4eed64b53a697
moghya <sawantshubham571@gmail.com>
2022-02-12 14:36:12 +0100

    Update KeyValueStoreApp to use OperationResult type

--- key_value_store_app/app/key_value_store_app.h
+++ key_value_store_app/app/key_value_store_app.h
@@ -36,7 +36,7 @@ public:
         return utils::GetMessageBufferCapacity();
     }
 
-    key_value_store_app::KeyValueStoreAppCommand ParseMessage(char* message_str);
+    KeyValueStoreAppProto::Command ParseMessage(char* message_str);
 
     tcp_util::ACTION_ON_CONNECTION HandleMessage(
             std::shared_ptr<TCPMessage> tcp_message) override;
